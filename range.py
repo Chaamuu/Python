26c9ac3fc14efe9e061dd8a783ebb403fbd91c00
chaamu <125999531+chaamu@users.noreply.github.com>
2023-06-17 10:34:16 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,24 @@
+for value in range(1,10,2):
+    print(value,end='')
+print()
+for value in range(-1,-16,-3):
+    print(value,end='')
+print()
+for value in range(-10,0,1):
+    print(value,end='')
+print()
+for value in range(-5,0,1):
+    print(value,end='')
+print()
+for value in range(5,0,-1):
+    print(value,end='')
+print()
+for value in range(-1,-11,-1):
+    print(value,end='')
+print()
+for value in range(-10,0,2):
+    print(value,end='')
+print()
+for value in range(5,0,3):
+    print(value,end='')
+print()
