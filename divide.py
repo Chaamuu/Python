90da8b11a77f845d988cbf6ea9c38a413cf29f83
chaamu <125999531+chaamu@users.noreply.github.com>
2023-06-19 11:05:43 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,25 @@
+#write a program number is divisible by 7 or not
+num=int(input("enter the value"))
+if num%7==0:
+    print(f'{num} is divisible by 7')
+else:
+    print(f'{num} is not divisible by 7')
+print()
+
+
+#check the number is even or not
+print("check the number is even or not")
+num =int(input("enter the value"))
+if num%2==0:
+    print(f'{num} is even number')
+else:
+    print(f'{num} is not even number')
+print()
+
+#write a program that is divisible by 5 print hello otherwise print bye
+print("number is divisible by 5")
+num=int(input("enter the value"))
+if num%5==0:
+    print("HELLO")
+else:
+    print("BYE")
