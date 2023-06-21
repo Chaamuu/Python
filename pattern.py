d4e66705a16e1d5ebdb9875b2357c3bc87cad7ae
chaamu <125999531+chaamu@users.noreply.github.com>
2023-06-21 22:13:58 +0530

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,35 +1,24 @@
 for i in range(1,6):
     for j in range(1,6):
-        print(i,end='')
-    print()
-print()
-
-for i in range(-5,0,1):
-    for j in range(1,6):
-        print(i,end='')
-    print()
-print()
-
-for i in range(1,6):
-    for j in range(1,i+1):
-        print(i,end='')
-    print()
-print()
-
-for i in range(5,0,-1):
-    for j in range(1,i+1):
-        print(i,end='')
+        if j>=i:
+            print(j,end=' ')
+        else:
+            print(' ',end=' ')
     print()
 print()
 
 for i in range(1,6):
-    for j in range(1,i+1):
-        print(j,end='')
+    for j in range(5,0,-1):
+        if j<=i:
+            print(j,end=' ')
+        else:
+            print(' ',end=' ')
     print()
 print()
 
-for i in range(5,0,-1):
+num=1
+for i in range(1,6):
     for j in range(1,i+1):
-        print(j,end='')
+        print(num,end=' ')
+        num=num+1
     print()
-
