4ead7517f70fbb578c27df6810a2efc55d7a26bf
chaamu <125999531+chaamu@users.noreply.github.com>
2023-06-21 19:37:26 +0530

    Add files via upload

---
+++ nameslist.py
@@ -0,0 +1,17 @@
+namesList=["naresh","kishore","meghs","rashmi","radhika","ramesh"]
+for name in namesList:
+    print(name)
+print()
+
+print("display in reverse order")
+for name in namesList[::-1]:
+    print(name)
+print()
+
+for name in namesList[:2]:
+    print(name)
+print()
+
+for name in namesList[3:]:
+    print(name)
+print()
